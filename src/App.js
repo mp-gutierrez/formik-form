72f99f41cf70d6aa3464e54374e9ebff7347a224
Paz Gutierrez <mariapaz.gutierreza@gmail.com>
2022-04-28 17:13:04 +0200

    starter files creation

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import './App.css';
+import {useFormik} from 'formik'
+
+function App() {
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      email: '',
+      password: '',
+    },
+    onSubmit: values =>{
+      console.log('form:', values);
+    },
+    validate: values => {
+      let errors = {};
+      if(!values.name) errors.name = 'Required';
+      if(!values.email) errors.email = 'Required';
+      if(!values.password) errors.password = 'Required';
+      return errors;
+    }
+  });
+
+  return (
+    <div>
+      <form onSubmit={formik.handleSubmit}>
+        <div>Name</div>
+        <input type="text" name="name" onChange={formik.handleChange} value={formik.values.name}/>
+
+        {formik.errors.name ? <div style={{color: 'crimson'}}>{formik.errors.name}</div>: null}
+
+
+        <div>Email</div>
+        <input type="text" name="email" onChange={formik.handleChange} value={formik.values.email}/>
+        
+        {formik.errors.email ? <div style={{color: 'crimson'}}>{formik.errors.email}</div>: null}
+
+
+        <div>Password</div>
+        <input type="text" name="password" onChange={formik.handleChange} value={formik.values.password}/>
+
+        {formik.errors.password ? <div style={{color: 'crimson'}}>{formik.errors.password}</div>: null}
+
+
+        <button type='submit'>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
